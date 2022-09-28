f865aed1f41ba8634ad15482dbc246dcce6b1974
baserak <almaz.talduk@gmail.com>
2022-09-28 14:44:03 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,90 @@
+function bannerInstall(){
+  	echo "########################################################################################################"
+		echo "|                                                                                                      |"	
+		echo "| #### #   #  ####  ######    #   #     #       ####  ###### ###### ######  ##    #   #   #####  ##### |"
+		echo "|  ##  ##  # ##       ##     # #  #     #      ##     ##   # ##       ##    ## #  #  # #  ##  ## #     |"
+		echo "|  ##  # # #  ####    ##    ##  # #     #       ####  ##   # ####     ##    ## #  # ##  # #####  ####  |"
+		echo "|  ##  #  ##     ##   ##    ##### #     #          ## ##   # ##       ##    ## #  # ##### ##  ## #     |"
+		echo "| #### #   #  ####    ##    ##  # ####  ####    ####  ###### ##       ##     ## ##  ##  # ##  ## ##### |"	
+		echo "|                                                                                                      |"	
+		echo "########################################################################################################"
+}
+function bannerdonne(){
+echo -e "##############################"
+echo -e "| ####   #####  #   #  ##### |"
+echo -e "| #   #  #   #  ##  #  #     |"
+echo -e "| #   #  #   #  # # #  ####  |"
+echo -e "| #   #  #   #  #  ##  #     |"
+echo -e "| ####   #####  #   #  ##### |"
+echo -e "##############################"
+}
+function exitfun() {
+		echo "######################################"
+		echo "| ####  #   # ####   ####  #   # #### |"
+		echo "| #   #  # #  #      #   #  # #  #    |"
+		echo "| ####    #   ###    ####    #   ###  |"
+		echo "| #   #   #   #      #   #   #   #    |"
+		echo "| ####    #   ####   ####    #   #### |"
+		echo "######################################"
+		exit 0
+}
+
+function initialCheck() {
+	if ! isRoot; then
+		echo "Sorry, you need to run this as root"
+		exitfun
+	fi
+	if ! tunAvailable; then
+		echo "TUN is not available"
+		exitfun
+	fi
+	checkOS
+}
+
+function checkOS() {
+	
+	if [[ -e /etc/system-release ]]; then
+		source /etc/os-release
+		if [[ $ID == "fedora" || $ID_LIKE == "fedora" ]]; then
+			OS="fedora"
+		fi
+		if [[ $ID == "centos" || $ID == "rocky" || $ID == "almalinux" ]]; then
+			OS="centos"
+			if [[ ! $VERSION_ID =~ (7|8) ]]; then
+				echo "⚠️ Your version of CentOS is not supported."
+				echo ""
+				echo "The script only support CentOS 7 and CentOS 8."
+				echo ""
+				exitfun
+			fi
+		fi
+	else
+		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, Amazon Linux 2, Oracle Linux 8 or Arch Linux system"
+		exitfun
+	fi
+}
+function information(){
+  echo "To unban an IP: fail2ban-client unban xxx.xxx.xxx.xxx"
+  echo "systemctl status fail2ban"
+  echo "fail2ban-client status"
+  echo "fail2ban-client status sshd"
+}
+function installsoft() {
+yum install -y epel-release
+yum install -y fail2ban
+echo "
+[sshd] 
+enabled = true
+" > /etc/fail2ban/jail.local
+systemctl restart fail2ban
+yum install -y lsof 
+yum install -y nano 
+}
+
+initialCheck
+bannerInstall
+installsoft
+bannerdonne
+information
+exitfun
+
